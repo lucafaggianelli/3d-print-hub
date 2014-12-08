1508c58589a85ef14b5b454c9390e2fa67291143
Luca Faggianelli <luca.faggianelli@gmail.com>
2014-12-08 20:33:19 +0100

    doc adapter

--- src/com/tesladocet/threedprinting/printers/PrintModelAdapter.java
+++ src/com/tesladocet/threedprinting/printers/PrintModelAdapter.java
@@ -6,16 +6,35 @@ import android.os.ParcelFileDescriptor;
 import android.print.PageRange;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
 
 public class PrintModelAdapter extends PrintDocumentAdapter {
 
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+	
 	@Override
 	public void onLayout(PrintAttributes oldAttributes,
 			PrintAttributes newAttributes,
 			CancellationSignal cancellationSignal,
 			LayoutResultCallback callback, Bundle extras) {
-		// TODO Auto-generated method stub
-		
+
+		// Respond to cancellation request
+	    if (cancellationSignal.isCanceled()) {
+	        callback.onLayoutCancelled();
+	        return;
+	    }
+	    
+	    PrintDocumentInfo info = new PrintDocumentInfo
+                .Builder("print_output.pdf")
+                .setContentType(PrintDocumentInfo.CONTENT_TYPE_UNKNOWN)
+                .setPageCount(1)
+                .build();
+	    
+        // Content layout reflow is complete
+        callback.onLayoutFinished(info, true);
 	}
 
 	@Override
@@ -25,4 +44,8 @@ public class PrintModelAdapter extends PrintDocumentAdapter {
 		
 	}
 
+	@Override
+	public void onFinish() {
+		super.onFinish();
+	}
 }
